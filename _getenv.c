f0b6aea60d0d8c00a6f81d76c640aa91c6e21bb1
Andrea Angola <3469@holbertonschool.com>
2021-08-23 15:44:10 -0700

    Fix for getenv function operation

--- _getenv.c
+++ _getenv.c
@@ -1,41 +1,21 @@
 #include "shell.h"
-#include <string.h>
-
-extern char **environ;
 
 char *_getenv(char *var_env)
 {
 	int i = 0;
+	char *copy;
 	char *env;
 
 	while (environ[i] != NULL)
-	{
-		env = strtok(environ[i], "=");
+	{       copy = _strdup(environ[i]);
+		env = strtok(copy, "=");
 		if (_strcmp(env, var_env) == 0)
 		{
 			return (strtok(NULL, "="));
+			                
 		}
 		i++;
 	}
 	return ("Error");
-}#include "shell.h"
-#include <string.h>
-
-extern char **environ;
-
-char *_getenv(char *var_env)
-{
-	int i = 0;
-	char *env;
-
-	while (environ[i] != NULL)
-	{
-		env = strtok(environ[i], "=");
-		if (_strcmp(env, var_env) == 0)
-		{
-			return (strtok(NULL, "="));
-		}
-		i++;
-	}
-	return ("Error");
+	
 }
