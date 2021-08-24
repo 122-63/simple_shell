7e702304bef6666721739622bf14e4eac663aaad
Andrea Angola <3469@holbertonschool.com>
2021-08-24 13:34:35 -0700

    betty styles

--- _execute.c
+++ _execute.c
@@ -1,51 +1,29 @@
 #include "shell.h"
-
-int _execute(char **tokens)
+/**
+ * _execute - execute built in command
+ * @new_path: complete path
+ * @tokens: array with buffer split
+ * @filename: name of executable
+ * @counter: counter commands enter
+ * Return: s_exceve status
+ */
+int _execute(char *new_path, char *filename, char **tokens, int counter)
 {
-	int status_w, index = 0, s_execve, status = 0;
-	char *new_path, *PATH;
-	char **t_path;
-	struct stat st;
 	pid_t pid;
+	int s_execve = 0, status_w;
 
-	PATH = _getenv("PATH");
-	t_path = split_string(PATH, ":");
-	while (t_path[index] != NULL)
+	pid = fork();
+	if (pid != -1)
 	{
-		if (stat(tokens[0], &st) == -1)
+		if (pid == 0)
 		{
-			new_path = _strcat(t_path[index], "/");
-			new_path = _strcat(new_path, tokens[0]);
-			                
+			s_execve = execve(new_path, tokens, NULL);
+			if (s_execve == -1)
+				_print_error(filename, tokens[0], counter);
 		}
-		else
-			new_path = tokens[0];
-		if (stat(new_path, &st) == 0)
-		{
-			status = 1;
-			pid = fork();
-			if (pid != -1)
-			{
-				if (pid == 0)
-				{
-					s_execve = execve(new_path, tokens, NULL);
-					if (s_execve == -1)
-						perror("Error: ");
-					                                
-				}
-				wait(&status_w);
-				                        
-			}
-			else
-				perror("Error: ");
-			break;
-			                
-		}
-		index++;
-		        
+		wait(&status_w);
 	}
-	if (status == 0)
-		perror("Error ");
+	else
+		perror("Error: ");
 	return (s_execve);
-	
 }
