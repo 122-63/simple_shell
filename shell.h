f138c04950482f924672e3d50cfde9a08d24c21c
SandraHoyos <slhoyosr@gmail.com>
2021-08-24 17:47:07 -0500

    shell.h

--- shell.h
+++ shell.h
@@ -1,7 +1,6 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
@@ -10,21 +9,29 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stddef.h>
-
-
+extern char **environ;
 /**
- * noBuiltIn - command no built in
+ * struct noBuiltIn - command no built in
  * @command: name of command
+ * @command_function: function for to execute.
  */
 typedef struct noBuiltIn
 {
 	char *command;
+	void (*command_function)(void);
 } t_noBuiltIn;
-
-extern char **environ;
 char *_getenv(char *var_env);
 char **split_string(char *buffer, const char *del);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
+void _env(void);
+void _exit_(void);
+char *_strdup(char *str);
+int _built_in(char **tokens, char *filename, int counter);
+int _execute(char *new_path, char *filename, char **tokens, int counter);
+int _execute_nb(char **tokens, int counter);
+void _dot(void);
+size_t _strlen(char *s);
+void _print_error(char *filename, char *command, int counter);
 
 #endif
