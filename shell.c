e84f6f6ae3560dd93f0f50a1f4c615edb0ce1de7
Andrea Angola <3469@holbertonschool.com>
2021-08-24 14:00:41 -0700

    rename main.c to shell.c and style betty

---
+++ shell.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+/**
+ * main - shell
+ * @ac: counter arguments
+ * @av: arguments array
+ * Return: 1 | 0
+ */
+int main(int ac __attribute__((unused)), char *av[])
+{
+
+	char *buffer;
+	size_t b_size;
+	ssize_t len;
+	const char *del = " \t\r\n\a";
+	char **tokens;
+	int status = 0, counter = 0;
+
+	while (1)
+	{
+		counter++;
+		if ((isatty(fileno(stdin))))
+			write(STDOUT_FILENO, "$ ", 3);
+		len = getline(&buffer, &b_size, stdin);
+
+		if (len != -1)
+		{
+			if (buffer[0] == '\n')
+			{
+				write(STDOUT_FILENO, "", 1);
+				continue;
+			}
+			tokens = split_string(buffer, del);
+			status = _execute_nb(tokens, counter);
+			if (status == 0)
+				_built_in(tokens, av[0], counter);
+		}
+		else if (len == EOF)
+		{
+			free(buffer);
+			if (isatty(STDIN_FILENO) != 0)
+				write(STDOUT_FILENO, "\n", 1);
+			return (0);
+		}
+	}
+	return (0);
+}
