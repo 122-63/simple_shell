68a8e3d2c4d920d1d0fe32a1a3bc30fcb05d54a5
Andrea Angola <3469@holbertonschool.com>
2021-08-24 13:28:46 -0700

    betty styles

--- _env.c
+++ _env.c
@@ -1,18 +1,4 @@
 #include "shell.h"
-/**
- * _strlen - function the returns the length of s tring
- *
- * @s: char variable to find length
- * Return: length of a string
- */
-size_t _strlen_(char *s)
-{
-	int l;
-	for (l = 0; s[l] != '\0'; l++)
-		;
-	return (l);
-	
-}
 
 /**
  * _env - print a environment
@@ -24,12 +10,10 @@ void _env(void)
 
 	while (environ[i] != NULL)
 	{
-		size = _strlen_(environ[i]);
+		size = _strlen(environ[i]);
 		size = size + 1;
 		write(STDOUT_FILENO, environ[i], size);
 		write(STDOUT_FILENO, "\n", 1);
 		i++;
-		        
 	}
-	
 }
